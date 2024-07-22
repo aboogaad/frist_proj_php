d08f33662b4591272e8f4779bf2426eae34cbf93
mohamed abogad <147059043+aboogaad@users.noreply.github.com>
2024-07-23 01:15:09 +0300

    Delete online_library directory

--- online_library/includes/db.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-
-    if (mysqli_connect_errno()) {
-        echo 'failed'. mysqli_connect_errno();
-    }
--- online_library/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-        <p>&copy; 2024 Boga Online Library. All rights reserved.</p>
-    </footer>
